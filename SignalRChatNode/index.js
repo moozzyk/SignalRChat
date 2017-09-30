3228a10c537a41031c7f0f023acc969a9624ffb5
moozzyk <moozzyk@gmail.com>
2017-09-30 11:23:49 -0700

    Starting connection - not working (missing polyfills)

--- SignalRChatNode/index.js
+++ SignalRChatNode/index.js
@@ -1,8 +1,15 @@
 const readline = require('readline');
+const signalR = require('@aspnet/signalr-client');
 
 let rl = readline.createInterface(process.stdin, process.stdout);
 
 rl.question('Enter your name: ', name => {
   console.log(name);
-  rl.close();
+  
+  let connection = new signalR.HubConnection('http://localhost:5000/chat');
+  connection.start()
+    .catch(error => {
+      console.error(error);
+      rl.close();
+    });
 });
