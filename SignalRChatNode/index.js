0f7f71fbd765b41bcff427709f7f505bc02af82e
moozzyk <moozzyk@gmail.com>
2017-09-30 19:00:52 -0700

    Complete Node SignalR Chat (JavaScript)

--- SignalRChatNode/index.js
+++ SignalRChatNode/index.js
@@ -10,7 +10,25 @@ rl.question('Enter your name: ', name => {
   console.log(name);
 
   let connection = new signalR.HubConnection('http://localhost:5000/chat');
+
+  connection.on('broadcastMessage', (name, message) => {
+    console.log(`${name}: ${message}`);
+    rl.prompt(true);
+  });
+
   connection.start()
+    .then(() => {
+      rl.prompt();
+      rl.on('line', input => {
+        if (input === '!q') {
+          console.log('Stopping connection...');
+          connection.stop();
+          rl.close();
+          return;
+        }
+        connection.send('send', name, input);
+      });
+    })
     .catch(error => {
       console.error(error);
       rl.close();
