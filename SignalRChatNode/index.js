946f85d6ffa2b765188857acd5c7bbff794b2d38
moozzyk <moozzyk@gmail.com>
2017-09-30 18:31:38 -0700

    Adding required polyfills

--- SignalRChatNode/index.js
+++ SignalRChatNode/index.js
@@ -1,11 +1,14 @@
 const readline = require('readline');
 const signalR = require('@aspnet/signalr-client');
 
+XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+WebSocket = require('websocket').w3cwebsocket;
+
 let rl = readline.createInterface(process.stdin, process.stdout);
 
 rl.question('Enter your name: ', name => {
   console.log(name);
-  
+
   let connection = new signalR.HubConnection('http://localhost:5000/chat');
   connection.start()
     .catch(error => {
